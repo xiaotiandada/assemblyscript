974df51f86b40d260dec80da84a5a6aa4acd5c55
xiaotiandada <istianlei@qq.com>
2022-02-07 16:41:00 +0800

    feat: add log

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,23 @@
-// The entry file of your WebAssembly module.
+import * as console from "./myConsole";
 
 export function add(a: i32, b: i32): i32 {
+  console.log(`run add: ${a}, ${b}`);
   return a + b;
+}
+
+   
+/**
+ * @param {number} number
+ * @return {number}
+ */
+ export default function factorial(number: i32): i32 {
+  console.log(`factorial: ${number}`);
+
+  let result = 1;
+
+  for (let i: i32 = 2; i <= number; i += 1) {
+    result *= i;
+  }
+
+  return result;
 }
--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,9 @@
 const assert = require("assert");
-const myModule = require("..");
+const myModule = require("../index");
 assert.strictEqual(myModule.add(1, 2), 3);
 console.log("ok");
+
+console.time('factorial')
+const result = myModule.factorial(15)
+console.log('factorial result: ', result)
+console.timeEnd('factorial')
